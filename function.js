9084adec59e188ef80e6efde6261adf6fcb19765
dquan2910 <dongquanmaingoc2910@gmail.com>
2024-03-08 01:46:27 +0700

    newesr

--- function.js
+++ function.js
@@ -12,7 +12,7 @@ function copy() {
     }      
   }
 
-  function change(){
+  function change1(){
     if(document.getElementById("promolink").value == "")
     {
         document.getElementById("thongbao").innerHTML = "Vui lòng điền link.";
@@ -28,10 +28,56 @@ function copy() {
         else
         {
             var promotionId = promolink.substring(posOfPromotionId + 13, posOfSignature);
-            var signature =  promolink.substring(posOfSignature + 11);
+            var signature =  (promolink.substring(posOfSignature + 11)).substring(0,64);
             document.getElementById("thongbao").innerHTML = "";
             var bodyCode = "{\n\t\"voucher_promotionid\":" + promotionId + ",\n\t\"signature\":\"" + signature + "\",\n\t\"signature_source\":\"0\"\n}";
             
+            document.getElementById("bodycode").value = bodyCode;
+        }
+    }   
+  }
+  function change2(){
+    if(document.getElementById("promolink").value == "")
+    {
+        document.getElementById("thongbao").innerHTML = "Vui lòng điền link.";
+    }
+    else    
+    {
+        var promolink = document.getElementById("promolink").value;
+        var posOfPromotionId = promolink.indexOf("&promotionId=");
+        var posOfSignature = promolink.indexOf("&signature=");
+        if(posOfPromotionId == -1 || posOfSignature == -1){
+            document.getElementById("thongbao").innerHTML = "Đường dẫn có vẻ sai, vui lòng điền lại";
+        }
+        else
+        {
+            var promotionId = promolink.substring(posOfPromotionId + 13, posOfSignature);
+            var signature =  (promolink.substring(posOfSignature + 11)).substring(0,64);
+            document.getElementById("thongbao").innerHTML = "";
+            var bodyCode = "async function postData(url = \"\", data = {}) {    const response = await fetch(url, {\n method: \"POST\",        headers: {\n\"Content-Type\": \"application/json\",        },\n body: JSON.stringify(data),    });\n return response.json();}\n postData(\"/api/v2/voucher_wallet/save_voucher\", {\n\"voucher_promotionid\": " + promotionId +",   \"signature\": \"" + signature +"\",\n\"signature_source\": \"0\",    \"security_device_fingerprint\": sessionStorage[\'shopee_webUnique_ccd\'].replace(/\\\"/gi, \'\'),\n}).then((data) => {    console.log(data);\n});";
+            document.getElementById("bodycode").value = bodyCode;
+        }
+    }   
+  }
+  function change3(){
+    if(document.getElementById("promolink").value == "")
+    {
+        document.getElementById("thongbao").innerHTML = "Vui lòng điền link.";
+    }
+    else    
+    {
+        var promolink = document.getElementById("promolink").value;
+        var posOfPromotionId = promolink.indexOf("&promotionId=");
+        var posOfSignature = promolink.indexOf("&signature=");
+        if(posOfPromotionId == -1 || posOfSignature == -1){
+            document.getElementById("thongbao").innerHTML = "Đường dẫn có vẻ sai, vui lòng điền lại";
+        }
+        else
+        {
+            var promotionId = promolink.substring(posOfPromotionId + 13, posOfSignature);
+            var signature =  (promolink.substring(posOfSignature + 11)).substring(0,64);
+            document.getElementById("thongbao").innerHTML = "";
+            var bodyCode = promotionId +":"+signature;
             document.getElementById("bodycode").value = bodyCode;
         }
     }   
