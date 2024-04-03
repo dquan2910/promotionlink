c7e65842060c58c61c253f36ee8a7433ec2e136a
dquan2910 <114803883+dquan2910@users.noreply.github.com>
2024-04-04 02:31:00 +0700

    Update function.js

--- function.js
+++ function.js
@@ -54,7 +54,7 @@ function copy() {
             var promotionId = promolink.substring(posOfPromotionId + 13, posOfSignature);
             var signature =  (promolink.substring(posOfSignature + 11)).substring(0,64);
             document.getElementById("thongbao").innerHTML = "";
-            var bodyCode = "async function postData(url = \"\", data = {}) {    const response = await fetch(url, {\n method: \"POST\",        headers: {\n\"Content-Type\": \"application/json\",        },\n body: JSON.stringify(data),    });\n return response.json();}\n postData(\"/api/v2/voucher_wallet/save_voucher\", {\n\"voucher_promotionid\": " + promotionId +",   \"signature\": \"" + signature +"\",\n\"signature_source\": \"0\",    \"security_device_fingerprint\": sessionStorage[\'shopee_webUnique_ccd\'].replace(/\\\"/gi, \'\'),\n}).then((data) => {    console.log(data);\n});";
+            var bodyCode = "async function postData(url = \"\", data = {}) {    const response = await fetch(url, {\n method: \"POST\",        headers: {\n\"Content-Type\": \"application/json\",        },\n body: JSON.stringify(data),    });\n return response.json();}\n postData(\"/api/v2/voucher_wallet/save_voucher\", {\n\"voucher_promotionid\": " + promotionId +",   \"signature\": \"" + signature +"\",\n\"signature_source\": \"0\",\n}).then((data) => {    console.log(data);\n});";
             document.getElementById("bodycode").value = bodyCode;
         }
     }   
