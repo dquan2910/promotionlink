8a931df10d3c43574a8ad0f901a27725b7808e23
dquan2910 <dongquanmaingoc2910@gmail.com>
2025-05-04 10:30:48 +0700

    Update UI

--- function.js
+++ function.js
@@ -11,7 +11,7 @@ function copy() {
         document.getElementById("thongbao").innerHTML = "Đã copy!!!";
     }      
   }
-
+//Lưu bằng Postman
   function change1(){
     if(document.getElementById("promolink").value == "")
     {
@@ -35,6 +35,7 @@ function copy() {
         }
     }   
   }
+//Lưu bình thường (không hiện UI) Javascript
   function change2(){
     if(document.getElementById("promolink").value == "")
     {
@@ -62,6 +63,7 @@ function copy() {
         }
     }   
   }
+//Lưu bằng BOT
   function change3(){
     if(document.getElementById("promolink").value == "")
     {
@@ -85,12 +87,7 @@ function copy() {
         }
     }   
   }
-  function reset()
-  {
-      document.getElementById("promolink").value = "";
-      document.getElementById("bodycode").value = "";
-document.getElementById("thongbao").innerHTML = "";
-  }
+//Lưu bằng Javascript có UI
   function change4(){
     if(document.getElementById("promolink").value == "")
     {
@@ -118,15 +115,12 @@ var s = document.createElement('script'); s.type = 'text/javascript';
 s.text = finalScript;
 document.head.appendChild(s);
 });`;
-            if(document.getElementById("bodycode").value.indexOf(`fetch('https://dquan2910.github.io/promotionlink/script.js')`) == 0 && document.getElementById("bodycode").value.indexOf(promotionId) != -1)
-            {
-                bodyCode = "avascript: " + bodyCode
-            }
+            bodyCode = checkCheckedBox(bodyCode);
             document.getElementById("bodycode").value = bodyCode;
         }
     }   
   }
-
+ //Lưu thường (Nhiều link)
   function change5(){
     if(document.getElementById("promolink").value == "")
     {
@@ -184,6 +178,7 @@ document.head.appendChild(s);
                 s.text = finalScript;
                 document.head.appendChild(s);
                 });`;
+                bodyCode = checkCheckedBox(bodyCode);
                 document.getElementById("bodycode").value = bodyCode;
             }
             else
@@ -192,6 +187,7 @@ document.head.appendChild(s);
             }
     }   
   }
+  //Spam lưu (1 link)
   function change6(){
     if(document.getElementById("promolink").value == "")
     {
@@ -219,11 +215,103 @@ var s = document.createElement('script'); s.type = 'text/javascript';
 s.text = finalScript;
 document.head.appendChild(s);
 });`;
-            if(document.getElementById("bodycode").value.indexOf(`fetch('https://dquan2910.github.io/promotionlink/script3.js')`) == 0 && document.getElementById("bodycode").value.indexOf(promotionId) != -1)
-            {
-                bodyCode = "avascript: " + bodyCode
-            }
+            bodyCode = checkCheckedBox(bodyCode);
             document.getElementById("bodycode").value = bodyCode;
         }
     }   
   }
+  //Spam lưu (Nhiều link)
+  function change7(){
+    if(document.getElementById("promolink").value == "")
+    {
+        document.getElementById("thongbao").innerHTML = "Vui lòng điền link.";
+    }
+    else    
+    {
+        var promolink = document.getElementById("promolink").value;
+        var stringList = "";
+        var listVoucher = promolink.split(',');
+        var errList = "";
+        var count = 0;
+        var test = "";
+        var listLength = listVoucher.length;
+        for (i = 0; i < listLength; i++)
+        {
+            
+            var linkVoucher = listVoucher[i].trim();
+            var posOfPromotionId = linkVoucher.indexOf("&promotionId=");
+            var posOfSignature = linkVoucher.indexOf("&signature=");
+            if(posOfPromotionId == -1 || posOfSignature == -1)
+            {                
+                if(errList.length != 0) errList = errList + ", ";
+                var posOfLink = i + 1;
+                errList = errList + "Link " + posOfLink;
+                count++;
+                continue;
+            }
+            else
+            {
+                if(stringList.length != 0) stringList = stringList + ", ";
+                var promotionId = linkVoucher.substring(posOfPromotionId + 13, posOfSignature);
+                var signature =  (linkVoucher.substring(posOfSignature + 11)).substring(0,64);
+                var code = '"' + promotionId + ":" + signature + '"';
+                stringList = stringList + code;
+            }
+        }
+        if(count != listLength)
+            {
+                if(count > 0)
+                {
+                    errList = errList + " sai nên không thêm vào list";
+                    document.getElementById("thongbao").innerHTML = errList;
+                }
+                else 
+                {
+                    document.getElementById("thongbao").innerHTML = "";
+                }
+                var bodyCode =
+                `fetch('https://dquan2910.github.io/promotionlink/script4.js')
+                .then(response => response.text())
+                .then(scriptContent => {
+                var finalScript = scriptContent.replace('tempList', '${stringList}');
+                var s = document.createElement('script'); s.type = 'text/javascript';
+                s.text = finalScript;
+                document.head.appendChild(s);
+                });`;
+                document.getElementById("bodycode").value = bodyCode;
+            }
+            else
+            {
+                document.getElementById("thongbao").innerHTML = "Tất cả link voucher đều sai!!!";
+            }
+    }   
+  }
+    //reset
+    function reset()
+    {
+    document.getElementById("promolink").value = "";
+    document.getElementById("bodycode").value = "";
+    document.getElementById("thongbao").innerHTML = "";
+    }
+    function checkCheckedBox(bodyCode)
+    {
+        var checkBox = document.getElementById("checkForAdding");
+        if(checkBox.checked == true)
+            return 'avascript: ' + bodyCode;
+        return bodyCode;
+    }
+    function checkedValidate(element)
+    {
+        var bodyCode = document.getElementById("bodycode");
+        var valBodyCode = bodyCode.value;
+        if(element.checked == true && valBodyCode != "" && !valBodyCode.includes("avascript: "))
+        {
+            valBodyCode = "avascript: " + valBodyCode;
+            bodyCode.value = valBodyCode;
+        }
+        if(element.checked == false && valBodyCode != "" && valBodyCode.includes("avascript: "))
+        {
+            valBodyCode = valBodyCode.replace("avascript: ", "");
+            bodyCode.value = valBodyCode;
+        }
+    }
