531df98d0e064545c1c565aae4abcb4754246950
dquan2910 <dongquanmaingoc2910@gmail.com>
2025-05-04 10:54:18 +0700

    Update Font

--- script4.js
+++ script4.js
@@ -9,9 +9,8 @@ async function SaveVoucher() {
     listvoucher[1] = new Array(tempList);
 var total = listvoucher.length;
 var count = 0;
-var savedCount = 0;
 var new_window = window.open('');
-new_window.document.write('<h1>Danh sách thực hiện lưu voucher:<h1>');
+new_window.document.body.write('<h1>Danh sách thực hiện lưu voucher:<h1>');
 while(true)
 {
     try
@@ -20,59 +19,64 @@ while(true)
             var tempPos = item[0].indexOf(":");
             var id = item.substring(0, tempPos);
             var signature = item.substring(tempPos + 1);
-            await wait(300);
+            await wait(200);
             const data = await postData("/api/v2/voucher_wallet/save_voucher", {
                 "voucher_promotionid": id, "signature": signature,
                 "signature_source": "0",
                 });
             count++;
             var err = data.error;
-            if(err == 0)
+            var errMsg = data.error_msg;
+            if (err == 0)
             {
-                let date = new Date(data.data.voucher.collect_time * 1000); 
+                let date = new Date(data.data.voucher.collect_time * 1000);
                 var claimDate = date.toLocaleString("vi-VN", { timeZone: "Asia/Ho_Chi_Minh" });
-                new_window.document.write(`<h2 style = "background-color: #00FF00;">[${claimDate}] Lưu thành công: ${data.data.voucher.voucher_code} - ${data.data.voucher.percentage_used}% đã sử dụng</h2>`);
-                break;                   
-            }
-            else if(err == 5)
-            {
-                new_window.document.write(`<h2>[${count}]${data.error_msg}</h2>`);
+                new_window.document.body.insertAdjacentHTML('afterbegin', `<h2 style="background-color: #00FF00;">[${claimDate}] Lưu thành công: ${data.data.voucher.voucher_code} - ${data.data.voucher.percentage_used}% đã sử dụng</h2>`);
                 break;
             }
-            else if(err == 14)
+            else if (err == 5) //Đã lưu trước đó
             {
-                var invalidCode = data.data.invalid_message_code;
-                var errMsg = data.error_msg;
-                if(invalidCode == 4)
-                {
-                    new_window.document.write(`<h2>[${count}]${errMsg}</h2>`);
-                    continue;
-                }
-                else
-                {
-                    new_window.document.write(`<h2>[${count}]${errMsg}</h2>`);
-                    break;
-                }
+                new_window.document.body.insertAdjacentHTML('afterbegin', `<h2>[${count}]${data.error_msg}</h2>`);
+                break;
             }
-            else if(err == 10002)
+            else if (err == 14)
             {
-                new_window.document.write(`<h2>Voucher không hợp lệ</h2>`);
+            var invalidCode = data.data.invalid_message_code;
+            if (invalidCode == 4)
+            {
+                new_window.document.body.insertAdjacentHTML('afterbegin', `<h2>[${count}]${errMsg}</h2>`);
+                continue;
+            }
+            else if (invalidCode == 8) //Khung thời gian chưa bắt đầu
+            {
+                new_window.document.body.insertAdjacentHTML('afterbegin', `<h2>[${count}]${errMsg}</h2>`);
+                continue;
+            }
+            else
+            {
+                new_window.document.body.insertAdjacentHTML('afterbegin', `<h2>[${count}]${errMsg}</h2>`);
+                break;
+            }
+            }
+            else if (err == 10002)
+            {
+                new_window.document.body.insertAdjacentHTML('afterbegin', '<h2>Voucher không hợp lệ</h2>');
                 break;
             }
             else
             {
-                new_window.document.write(`<h2>[${count}]${err} - Đã dính captcha =)))</h2>`);
+                new_window.document.body.insertAdjacentHTML('afterbegin', `<h2>[${count}]${err} - Đã dính captcha</h2>`);
                 break;
             }
         }
     }
-    catch(error)
+    catch (error)
     {
-        new_window.document.write('Đã dính captcha =))');
+        new_window.document.body.insertAdjacentHTML('afterbegin', `<h2>Đã dính captcha</h2>`);
         break;
     }
 }
-    
-    new_window.document.write(`<h1 style = "color: red;">Kết quả: ${count}/${total} voucher đã lưu thành công</h1>`);
+    var savedCount = 0;
+    new_window.document.body.insertAdjacentHTML('afterbegin',`<h1 style = "color: red;">Kết quả: ${savedCount}/${total} voucher đã lưu thành công</h1>`);
 }
 SaveVoucher();
